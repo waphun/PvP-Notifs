a2215475ea14d3977b7437f0256812148da108ec
waphun <57304346+waphun@users.noreply.github.com>
2022-08-07 14:28:32 +0200

    no

--- scripts/main.js
+++ scripts/main.js
@@ -1004,7 +1004,7 @@ Events.run(Trigger.draw, () => {
 					}
 					var tb = tile.drop();
 					if(tb){
-						Draw.rect(tb.icon(defaultContentIcons.small),tile.drawx(),tile.drawy()); //Cicon
+						Draw.rect(tb.uiIcon,tile.drawx(),tile.drawy()); //Cicon
 					}
 				}
 			}
